e4044d1fab36402f2af9143bec893e840835cb35
byron fisher <fbyron710@gmnail.com>
2022-10-06 20:14:13 -0400

    music

---
+++ app.js
@@ -0,0 +1,30 @@
+module.exports =
+ [{  albumNumber: "0",
+    name:"Graduation",
+    publishDate:"2007",
+    imgURL:"https://i.pinimg.com/564x/b6/e7/dd/b6e7ddc2ad07eeb19872049cbbd98e3e--kanye-west-graduation-hiphop.jpg",
+   songTitles:["Good Morning","Stronger", "Good Life", "Champion"]
+ },
+ {
+    albumNumber:"1",
+    name:"Late Registration",
+    description:"2005",
+    imgURL:"https://images.genius.com/69d0a1c18ffbe8d1cabd15fbc2c87dfc.755x597x1.jpg",
+   songTitles:["Wake Up Mr. West","Heard Em Say", "Gold Digger", "Drive Show"]
+ }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
